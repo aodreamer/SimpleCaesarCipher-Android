205e358cce740d01762d26a327ed8fa98416eff1
aodreamer <nael.ano101"gmail.com>
2022-10-22 11:18:39 +0700

    Add Text

--- app/src/main/java/com/example/simplecesarcipher/MainActivity.kt
+++ app/src/main/java/com/example/simplecesarcipher/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.simplecesarcipher
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +12,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.simplecesarcipher.ui.theme.SimpleCesarCipherTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,23 +24,30 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                ){WelcomeText("Welcome to My App","- Developer")}
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WelcomeText(message: String, from: String){
+    Column{
+        Text(
+            text = message,
+            fontSize = 36.sp
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun SimpleCaesarPreview() {
     SimpleCesarCipherTheme {
-        Greeting("Android")
+        WelcomeText("Welcome to My App","- Developer")
     }
 }
