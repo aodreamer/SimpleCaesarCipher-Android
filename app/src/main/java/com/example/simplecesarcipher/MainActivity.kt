128849d92e1d9fc3c824ad38484021aaec887cc7
Nathanael Berliano <nael.ano101@gmail.com>
2022-10-24 15:09:11 +0700

    Testing using base64 decode and add copy button

--- app/src/main/java/com/example/simplecesarcipher/MainActivity.kt
+++ app/src/main/java/com/example/simplecesarcipher/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.example.simplecesarcipher
 
 
+import android.content.ClipData
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,8 +20,17 @@ import com.example.simplecesarcipher.ui.theme.SimpleCesarCipherTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.font.FontWeight
+import java.util.*
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.text.AnnotatedString
+import androidx.core.content.ContextCompat.getSystemService
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -37,17 +45,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview
 @Composable
 fun TipTimeScreen(){
     var amountInput by remember {mutableStateOf("")}
     val tip = calc(amountInput)
+    val clipboardManager: androidx.compose.ui.platform.ClipboardManager = LocalClipboardManager.current
     Column(
         modifier = Modifier.padding(32.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ){
         Text(
-            text = stringResource(R.string.calculate_tip),
+            text = stringResource(R.string.Base64_App),
             fontSize = 24.sp,
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
@@ -56,11 +66,16 @@ fun TipTimeScreen(){
             onValueChange = {amountInput = it} )
         Spacer(Modifier.height(25.dp))
         Text(
-            text = stringResource(R.string.tip_amount,tip),
+            text = stringResource(R.string.Result,tip),
             modifier = Modifier.align(Alignment.CenterHorizontally),
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold
         )
+        Button(onClick = {
+            clipboardManager.setText(AnnotatedString((tip)))
+        }) {
+            Text("Copy")
+        }
     }
 
 }
@@ -72,17 +87,19 @@ fun EditNumberField(value: String,
     TextField(
         value = value,
         onValueChange = onValueChange,
-        label = { Text(stringResource(R.string.cost_of_service)) },
+        label = { Text(stringResource(R.string.Input_Here)) },
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,
     )
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 private fun calc(input: String): String {
-    val out = input
+    val out = Base64.getEncoder().encodeToString(input.toByteArray())
     return out
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview(){
